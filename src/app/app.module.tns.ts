e8157613ce1ee26e196e82c106cc3390e5da2de2
Robert Beier <r.beier199@gmail.com>
2018-11-05 08:25:40 +0100

    Add mobile side drawer, make status bar transparent

--- src/app/app.module.tns.ts
+++ src/app/app.module.tns.ts
@@ -1,16 +1,19 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { NativeScriptUISideDrawerModule } from 'nativescript-ui-sidedrawer/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-
+import { setStatusBarColors } from './shared';
 
 // Uncomment and add to NgModule imports if you need to use two-way binding
 // import { NativeScriptFormsModule } from 'nativescript-angular/forms';
 
 // Uncomment and add to NgModule imports  if you need to use the HTTP wrapper
 // import { NativeScriptHttpClientModule } from 'nativescript-angular/http-client';
+
+setStatusBarColors();
 
 @NgModule({
   declarations: [
@@ -20,6 +23,7 @@ import { HomeComponent } from './home/home.component';
   imports: [
     NativeScriptModule,
     AppRoutingModule,
+    NativeScriptUISideDrawerModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
