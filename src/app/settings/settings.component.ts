87b985b7f9fa09abb9d29c2e19814b47b6bcebbb
Robert Beier <r.beier199@gmail.com>
2018-12-05 01:47:27 +0100

    Add some user information to settings page

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatDialog, MatSnackBar } from '@angular/material';
 
 import { AuthService } from '../shared/auth/auth.service';
 import { SidenavService } from '../shared/sidenav/sidenav.service';
+import { UserService } from '../shared/user/user.service';
+import { User } from '../shared/user/user.model';
+import { StoreHashInfoComponent } from '../shared/store-hash-info/store-hash-info.component';
+import { InitialPollingService } from '../shared/initial-polling/initial-polling.service';
 
 @Component({
   selector: 'p4ba-settings',
@@ -11,13 +16,27 @@ import { SidenavService } from '../shared/sidenav/sidenav.service';
 })
 export class SettingsComponent implements OnInit {
 
+  user: User;
+
   constructor(
     private authService: AuthService,
     private router: Router,
     private sidenavService: SidenavService,
-  ) { }
+    private userService: UserService,
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar,
+    private initialPollingService: InitialPollingService,
+  ) {
+    this.userService.getData().subscribe((user: User) => this.user = user);
+  }
 
   ngOnInit() {
+  }
+
+  openHashHelpDialog(): void {
+    const dialogRef = this.dialog.open(StoreHashInfoComponent, {
+      maxWidth: 600
+    });
   }
 
   logout() {
