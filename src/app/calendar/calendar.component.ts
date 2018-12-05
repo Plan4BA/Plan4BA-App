aad5feaef4ab2548ba659bcf9bf3d09a403610fd
Robert Beier <r.beier199@gmail.com>
2018-12-05 15:12:41 +0100

    Improve scrolling behaviour on calendar page on small screens

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,23 +1,50 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'p4ba-calendar',
   templateUrl: './calendar.component.html',
   styleUrls: ['./calendar.component.scss']
 })
-export class CalendarComponent {
+export class CalendarComponent implements AfterViewInit {
 
   viewDate: Date = new Date();
+  @ViewChild('monthlyCalendar') monthlyCalendar;
   @ViewChild('dailyCalendar') dailyCalendar;
+  private blockScroll = false;
 
   constructor(
     public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute,
     ) { }
+
+  ngAfterViewInit() {
+    this.route.fragment.subscribe((fragment: String) => {
+      if (!this.blockScroll) {
+        if (fragment === 'dailyCalendar') {
+          this.dailyCalendar.nativeElement.scrollIntoView(true);
+        } else {
+          this.monthlyCalendar.nativeElement.scrollIntoView(true);
+        }
+      }
+    });
+  }
+
 
   dayClickedListener(date: Date): void {
     this.viewDate = date;
-    setTimeout(() => this.dailyCalendar.nativeElement.scrollIntoView(true), 0);
+    this.blockScroll = true;
+    this.router.navigate([], {
+      fragment: ''
+    });
+    setTimeout(() => {
+      this.blockScroll = false;
+      this.router.navigate([], {
+        fragment: 'dailyCalendar'
+      });
+    }, 0);
   }
 
   prevMonth(): void {
