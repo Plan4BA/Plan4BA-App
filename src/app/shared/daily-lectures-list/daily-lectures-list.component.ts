6f493a441f4d90a1d1e57149545b1fc4bce3cdee
Robert Beier <r.beier199@gmail.com>
2018-12-05 20:15:10 +0100

    Dont filter lectures starting or ending at midnight

--- src/app/shared/daily-lectures-list/daily-lectures-list.component.ts
+++ src/app/shared/daily-lectures-list/daily-lectures-list.component.ts
@@ -48,9 +48,9 @@ export class DailyLecturesListComponent implements OnInit {
     nextDay.setUTCDate(nextDay.getUTCDate() + 1);
     const nextDayTimestamp = nextDay.getTime();
     return lectures.filter((lecture: Lecture) => {
-      return lecture.start * 1000 > dateTimestamp
+      return lecture.start * 1000 >= dateTimestamp
         && lecture.start * 1000 < nextDayTimestamp
-        && lecture.end * 1000 > dateTimestamp
+        && lecture.end * 1000 >= dateTimestamp
         && lecture.end * 1000 < nextDayTimestamp;
     }).sort((lectureA: Lecture, lectureB: Lecture) =>  lectureA.start - lectureB.start);
   }
