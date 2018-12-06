1add26e4bc423ff821c5dabb2e9f641b6e25763a
Robert Beier <r.beier199@gmail.com>
2018-12-06 19:59:59 +0100

    Revert "Remove inheritance structure used for nativescript"
    This reverts commit d1a21065f630e1f70084ce950a7eabfacf955d01.

---
+++ src/app/login/login-common.component.ts
@@ -0,0 +1,59 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../shared/auth/auth.service';
+import { alert } from '../shared/dialog-util/dialog-util';
+
+export class LoginCommonComponent {
+
+  private _isAuthenticating = false;
+
+  // username and password will be set with forms
+  username = '';
+  password = '';
+  storeHash = false;
+  acceptPrivacy = false;
+
+  constructor(
+    protected authService: AuthService,
+    protected router: Router,
+    ) { }
+
+  get isAuthenticating(): boolean {
+    return this._isAuthenticating;
+  }
+
+  set isAuthenticating(isAuthenticating: boolean) {
+    this._isAuthenticating = isAuthenticating;
+  }
+
+  login() {
+    if (!this.username || !this.password) {
+      alert('Please enter a username and a password!');
+      return;
+    }
+    if (!this.acceptPrivacy) {
+      alert('Please accept the privacy conditions!');
+      return;
+    }
+    this.isAuthenticating = true;
+    const loginSub = this.authService.login(this.username, this.password, this.storeHash)
+      .subscribe(
+        () => {
+          this.isAuthenticating = false;
+          loginSub.unsubscribe();
+          this.router.navigate(['/']);
+        },
+        (error) => {
+          // status code 401 means "unauthorized"
+          this.isAuthenticating = false;
+          loginSub.unsubscribe();
+          if (error.status === 401) {
+            alert('The combination of username and password don\'t match an account.');
+          } else {
+            alert('An unknown error occured while trying to log in.');
+          }
+        }
+      );
+  }
+}
