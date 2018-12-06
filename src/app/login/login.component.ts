959d3653da63d2e9129944c548f33affde485ae2
Robert Beier <r.beier199@gmail.com>
2018-12-06 20:04:38 +0100

    Remove inheritance structure used for nativescript

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,7 +4,6 @@ import { MatDialog } from '@angular/material';
 
 import { AuthService } from '../shared/auth/auth.service';
 import { alert } from '../shared/dialog-util/dialog-util';
-import { LoginCommonComponent } from './login-common.component';
 import { StoreHashInfoComponent } from '../shared/store-hash-info/store-hash-info.component';
 import { LoginInfoComponent } from '../shared/login-info/login-info.component';
 
@@ -13,14 +12,58 @@ import { LoginInfoComponent } from '../shared/login-info/login-info.component';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent extends LoginCommonComponent {
+export class LoginComponent {
+
+  private _isAuthenticating = false;
+
+  // username and password will be set with forms
+  username = '';
+  password = '';
+  storeHash = false;
+  acceptPrivacy = false;
 
   constructor(
-    authService: AuthService,
-    router: Router,
+    private authService: AuthService,
+    private router: Router,
     private dialog: MatDialog,
-    ) {
-      super(authService, router);
+    ) { }
+
+  get isAuthenticating(): boolean {
+    return this._isAuthenticating;
+  }
+
+  set isAuthenticating(isAuthenticating: boolean) {
+    this._isAuthenticating = isAuthenticating;
+  }
+
+  login() {
+    if (!this.username || !this.password) {
+      alert('Please enter a username and a password!');
+      return;
+    }
+    if (!this.acceptPrivacy) {
+      alert('Please accept the privacy conditions!');
+      return;
+    }
+    this.isAuthenticating = true;
+    const loginSub = this.authService.login(this.username, this.password, this.storeHash)
+      .subscribe(
+        () => {
+          this.isAuthenticating = false;
+          loginSub.unsubscribe();
+          this.router.navigate(['/']);
+        },
+        (error) => {
+          // status code 401 means "unauthorized"
+          this.isAuthenticating = false;
+          loginSub.unsubscribe();
+          if (error.status === 401) {
+            alert('The combination of username and password don\'t match an account.');
+          } else {
+            alert('An unknown error occured while trying to log in.');
+          }
+        }
+      );
   }
 
   openHashHelpDialog(): void {
