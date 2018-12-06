c6c0af7855093a990ec7ce38f7dbca219f1aceb6
Robert Beier <r.beier199@gmail.com>
2018-12-06 18:55:11 +0100

    Fix blocking token interceptor

--- src/app/shared/token-interceptor/token-interceptor.service.ts
+++ src/app/shared/token-interceptor/token-interceptor.service.ts
@@ -23,49 +23,53 @@ export class TokenInterceptorService {
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    if (request.url.indexOf(environment.apiUrl + 'token') === 0) {
-      return next.handle(request).pipe(
-        catchError((error: HttpErrorResponse) => {
-          if (error.status === 401) {
-            if (!this.router) {
-              this.router = this.injector.get(Router);
+    if (request.url.indexOf(environment.apiUrl) === 0) {
+      if (request.url.indexOf(environment.apiUrl + 'token') === 0) {
+        return next.handle(request).pipe(
+          catchError((error: HttpErrorResponse) => {
+            if (error.status === 401) {
+              if (!this.router) {
+                this.router = this.injector.get(Router);
+              }
+              this.authService.logout();
+              this.router.navigate(['/login']);
             }
-            this.authService.logout();
-            this.router.navigate(['/login']);
-          }
-          return throwError(error);
-        })
-      );
-    } else if (
-      request.headers.has('Authorization')
-      || request.url.indexOf(environment.apiUrl + 'info') === 0
-    ) {
-      return next.handle(request);
-    } else {
-      // this dynamic injection is required to not create a cyclic dependency
-      // https://github.com/angular/angular/issues/18224
-      if (!this.authService) {
-        this.authService = this.injector.get(AuthService);
-      }
-      return this.authService.authTokenData.pipe(
-        filter(tokenData => !!tokenData),
-        switchMap(tokenData => {
-          return next.handle(this.addAuthToken(request, tokenData.token));
-        }),
-        catchError((error: any) => {
-          if (error instanceof HttpErrorResponse && error.status === 401) {
-            this.authService.refreshAuthToken();
-            return this.authService.authTokenData.pipe(
-              filter(tokenData => !!tokenData),
-              switchMap(tokenData => {
-                return next.handle(this.addAuthToken(request, tokenData.token));
-              })
-            );
-          } else {
             return throwError(error);
-          }
-        })
-      );
+          })
+        );
+      } else if (
+        request.headers.has('Authorization')
+        || request.url.indexOf(environment.apiUrl + 'info') === 0
+      ) {
+        return next.handle(request);
+      } else {
+        // this dynamic injection is required to not create a cyclic dependency
+        // https://github.com/angular/angular/issues/18224
+        if (!this.authService) {
+          this.authService = this.injector.get(AuthService);
+        }
+        return this.authService.authTokenData.pipe(
+          filter(tokenData => !!tokenData),
+          switchMap(tokenData => {
+            return next.handle(this.addAuthToken(request, tokenData.token));
+          }),
+          catchError((error: any) => {
+            if (error instanceof HttpErrorResponse && error.status === 401) {
+              this.authService.refreshAuthToken();
+              return this.authService.authTokenData.pipe(
+                filter(tokenData => !!tokenData),
+                switchMap(tokenData => {
+                  return next.handle(this.addAuthToken(request, tokenData.token));
+                })
+              );
+            } else {
+              return throwError(error);
+            }
+          })
+        );
+      }
+    } else {
+      return next.handle(request);
     }
   }
 
