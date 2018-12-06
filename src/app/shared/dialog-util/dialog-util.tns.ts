97663adbf4bc8dfa3bb51b1aa2073ce653560d59
Robert Beier <r.beier199@gmail.com>
2018-12-06 03:09:06 +0100

    Remove nativescript from project

--- src/app/shared/dialog-util/dialog-util.tns.ts
+++
@@ -1,9 +0,0 @@
-import * as dialogsModule from 'ui/dialogs';
-
-export function alert(message: string): void {
- dialogsModule.alert({
-    title: 'Plan4BA',
-    okButtonText: 'OK',
-    message: message
-  });
-}
--- src/environments/environment.prod.tns.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://plan4ba.ddkfm.de/api/'
-};
