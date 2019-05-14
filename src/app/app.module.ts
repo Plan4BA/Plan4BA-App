f245c19af1f8ca23d4161df121d0d9ea5418035e
Robert Beier <30528168+Robert-Beier@users.noreply.github.com>
2019-05-14 22:07:57 +0200

    Deactivate sentry in development mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -58,11 +58,13 @@ export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
 }
 
-Sentry.init({
-  dsn: 'https://a16c84b452b04e7298dacf3e9ef127f0@sentry.plan4ba.ba-leipzig.de//2',
-  environment: environment.production ? 'prod' : 'dev',
-  release: 'plan4ba@0.3.0'
-});
+if (environment.production) {
+  Sentry.init({
+    dsn: 'https://a16c84b452b04e7298dacf3e9ef127f0@sentry.plan4ba.ba-leipzig.de//2',
+    environment: environment.production ? 'prod' : 'dev',
+    release: 'plan4ba@0.3.0'
+  });
+}
 
 @Injectable()
 export class SentryErrorHandler implements ErrorHandler {
@@ -127,7 +129,7 @@ export class SentryErrorHandler implements ErrorHandler {
       },
       deps: [TranslateService]
     },
-    { provide: ErrorHandler, useClass: SentryErrorHandler },
+    ...(environment.production ? [{ provide: ErrorHandler, useClass: SentryErrorHandler }] : []),
     LecturesService,
     MealsService,
     authProviders,
