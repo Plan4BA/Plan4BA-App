4434f0eb30ad3c5db2bacea1a40d81339c7a170e
Robert Beier <30528168+Robert-Beier@users.noreply.github.com>
2019-05-14 22:31:53 +0200

    Add missing decorator to a provider

--- src/app/shared/monthly-calendar/custom-date-formatter.provider.ts
+++ src/app/shared/monthly-calendar/custom-date-formatter.provider.ts
@@ -1,6 +1,10 @@
 import { CalendarDateFormatter, DateFormatterParams } from 'angular-calendar';
 import { DatePipe } from '@angular/common';
+import { Injectable } from '@angular/core';
 
+@Injectable({
+  providedIn: 'root',
+})
 export class CustomDateFormatter extends CalendarDateFormatter {
 
   public monthViewColumnHeader({ date, locale }: DateFormatterParams): string {
