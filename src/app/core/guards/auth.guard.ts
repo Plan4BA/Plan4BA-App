43dc690dc52a9fdf25225cd0264b39f6ce40d3c4
Robert Beier <30528168+Robert-Beier@users.noreply.github.com>
2019-05-24 12:31:19 +0200

    Fix auth guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -11,7 +11,7 @@ export class AuthGuard implements CanActivate {
   constructor(private router: Router, private authService: AuthService) {}
 
   canActivate(): boolean {
-    if (this.authService.isLoggedIn) {
+    if (this.authService.isLoggedIn.getValue()) {
       return true;
     } else {
       this.router.navigate(['/login']);
